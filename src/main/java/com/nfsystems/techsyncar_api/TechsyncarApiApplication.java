bfa32137288ae12b370561ab6ea3cbe3fb649691
Nilton F <nfdeveloper04@gmail.com>
2025-12-05 18:45:58 -0300

    primeiro commit criacao do projeto

---
+++ src/main/java/com/nfsystems/techsyncar_api/TechsyncarApiApplication.java
@@ -0,0 +1,13 @@
+package com.nfsystems.techsyncar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsyncarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsyncarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nfsystems/techsyncar_api/TechsyncarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfsystems.techsyncar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsyncarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
